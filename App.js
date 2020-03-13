a7db6f59589846451ebdb461365a04edc23ff0ff
n1kko777 <3734489@gmail.com>
2020-03-13 21:37:57 +0500

    Toggle Theme

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { mapping, light as lightTheme } from "@eva-design/eva";
+import { mapping, light, dark } from "@eva-design/eva";
+
 import { default as appTheme } from "./src/themes/custom-theme.json";
+import { ThemeContext } from "./src/themes/theme-context";
+
 import { AppNavigator } from "./src/navigations/AppNavigator";
 
-const theme = { ...lightTheme, ...appTheme };
+const themes = { light, dark };
 
-const App = () => (
-  <React.Fragment>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider mapping={mapping} theme={theme}>
-      <AppNavigator />
-    </ApplicationProvider>
-  </React.Fragment>
-);
+const App = () => {
+  const [theme, setTheme] = React.useState("light");
+  const currentTheme = { ...themes[theme], ...appTheme };
+
+  const toggleTheme = () => {
+    const nextTheme = theme === "light" ? "dark" : "light";
+    setTheme(nextTheme);
+  };
+
+  return (
+    <React.Fragment>
+      <IconRegistry icons={EvaIconsPack} />
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ApplicationProvider mapping={mapping} theme={currentTheme}>
+          <AppNavigator />
+        </ApplicationProvider>
+      </ThemeContext.Provider>
+    </React.Fragment>
+  );
+};
 
 export default App;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { Button, Divider, Layout, TopNavigation } from "@ui-kitten/components";
+import { ThemeContext } from "../themes/theme-context";
 
 export const HomeScreen = ({ navigation }) => {
+  const themeContext = React.useContext(ThemeContext);
+
   const navigateLogin = () => {
     navigation.navigate("Login");
   };
@@ -18,8 +21,19 @@ export const HomeScreen = ({ navigation }) => {
       <Layout
         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
-        <Button onPress={navigateLogin}>Login Screen</Button>
-        <Button onPress={navigateRegister}>Register Screen</Button>
+        <Button style={{ marginVertical: 4 }} onPress={navigateLogin}>
+          Login Screen
+        </Button>
+        <Button style={{ marginVertical: 4 }} onPress={navigateRegister}>
+          Register Screen
+        </Button>
+
+        <Button
+          style={{ marginVertical: 4 }}
+          onPress={themeContext.toggleTheme}
+        >
+          TOGGLE THEME
+        </Button>
       </Layout>
     </SafeAreaView>
   );
