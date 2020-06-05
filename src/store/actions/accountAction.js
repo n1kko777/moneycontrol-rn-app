b6abbd2c7a946daf79f23ff346ed0c3800f30a2d
n1kko777 <3734489@gmail.com>
2020-06-05 16:41:08 +0500

    Баланс 0

--- src/store/actions/accountAction.js
+++ src/store/actions/accountAction.js
@@ -113,11 +113,11 @@ export const updateAccount = (id, account) => async (dispatch) => {
 
 // Delete account from server
 export const hideAccount = (account) => async (dispatch) => {
-  if (account.balance > 0) {
+  if (account.balance == 0) {
     dispatch(
       accountFail({
         custom: {
-          title: "Баланс счета больше нуля!!",
+          title: "Баланс счета не равен 0!!",
           message: "Пожалуйста, переведи средства на другой счет.",
         },
       })
