0cecf519aff30ecc7843749793e19b9e32bc69af
n1kko777 <3734489@gmail.com>
2021-02-12 15:21:44 +0500

    New operation ListItem View

--- src/components/operation/OperationList.js
+++ src/components/operation/OperationList.js
@@ -2,18 +2,21 @@ import React, { memo } from "react";
 import { RefreshControl, SectionList } from "react-native";
 import { OperationListItem } from "./OperationListItem";
 import { Text } from "@ui-kitten/components";
+import { View } from "react-native";
 
 export const OperationList = memo(
   ({ navigation, dataList = [], onOperationRefresh }) => (
     <SectionList
-      style={{ flex: 1, height: "100%" }}
       scrollEnabled
-      stickySectionHeadersEnabled
+      stickySectionHeadersEnabled={false}
       initialNumToRender={3}
       sections={dataList}
       keyExtractor={(item, index) => item + index}
+      nestedScrollEnabled={true}
       renderItem={({ item }) => (
-        <OperationListItem item={item} navigation={navigation} />
+        <View style={{ marginVertical: 4 }}>
+          <OperationListItem item={item} navigation={navigation} />
+        </View>
       )}
       renderSectionHeader={({ section: { title } }) => (
         <Text
